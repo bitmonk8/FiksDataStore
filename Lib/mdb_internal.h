0692f25e5574347952d2a20dec109254a7eb2be7
Thomas Andersen <thomasa@unity3d.com>
2025-06-18 10:30:42 +0200

    add missing forward declarations

--- Lib/mdb_internal.h
+++ Lib/mdb_internal.h
@@ -1600,3 +1600,7 @@ extern pthread_key_t mdb_tls_keys[MAX_TLS_KEYS];
 extern int mdb_tls_nkeys;
 #endif
 
+int ESECT mdb_fopen(const MDB_env *env, MDB_name *fname, enum mdb_fopen_type which, mdb_mode_t mode, HANDLE *res);
+int ESECT mdb_fname_init(const char *path, unsigned envflags, MDB_name *fname);
+void _mdb_txn_abort(MDB_txn *txn);
+int mdb_txn_renew0(MDB_txn *txn);
