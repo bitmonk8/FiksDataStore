c0ac146b403dcb784fb44fe7fa036efe8ab19ed2
Thomas Andersen <thomas.andersen@gmail.com>
2025-06-18 16:54:56 +0200

    Fixie

--- Lib/mdb_txn.c
+++ Lib/mdb_txn.c
@@ -4,6 +4,7 @@
 #include "mdb_env.h"
 #include "mdb_lock.h"
 #include "mdb_debug.h"
+#include "mdb_cursor.h"
 
 /** Common code for #mdb_txn_begin() and #mdb_txn_renew().
  * @param[in] txn the transaction handle to initialize
